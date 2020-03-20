853d189ebb2da18b43aa0d0fc393daa351eb1f4f
wangtanke <1665497618@qq.com>
2020-03-20 22:05:50 +0800

    mdf

--- src/views/detail/childrenCom/GoodsShow.vue
+++ src/views/detail/childrenCom/GoodsShow.vue
@@ -1,35 +1,40 @@
 <template>
   <div v-if="imgList">
-    <img v-for="(item,index) in imgList" :key="index+'goodsShow'" :src="item" @load="imgLoad">
+    <img
+      v-for="(item, index) in imgList"
+      :key="index + 'goodsShow'"
+      :src="item"
+      @load="imgLoad"
+    />
   </div>
 </template>
 <script>
 export default {
-  name:'GoodsShow',
-  props:{
-    imgList:{
-      type:Array,
-      default(){
-        return []
+  name: "GoodsShow",
+  props: {
+    imgList: {
+      type: Array,
+      default() {
+        return [];
       }
     }
   },
   data() {
     return {
-      currentIndex:0
-    }
+      currentIndex: 0
+    };
   },
   methods: {
-    imgLoad(){
-      if(++currentIndex == imgList.length){
-        this.$emit('goodsShowLoad')
+    imgLoad() {
+      if (++this.currentIndex == this.imgList.length) {
+        this.$emit("goodsShowLoad");
       }
     }
-  },
-}
+  }
+};
 </script>
 <style scoped>
-img{
+img {
   width: 96%;
 }
 </style>
