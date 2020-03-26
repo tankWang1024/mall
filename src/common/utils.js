2ff98d01fb52fe7d9651f9f96c9ca237f5f103f3
wangtanke <1665497618@qq.com>
2020-03-26 14:36:53 +0800

    add utils throttle

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,17 +1,26 @@
 //方法
+
+// 防抖动
 export function debounce(func, delay) {
   // 防抖动函数,下次执行前等待时间大于等于delay时func才被执行
   // 持久化timer变量,只执行一次,然后拿返回的函数来执行
   let timer = null;
   return function (...args) {
-    /**
-     * 事件循环机制
-     * 同步在主线程执行完后,再把异步推入主线程执行,
-     * 所以频繁事件下次执行时,若未超过设置的delay,则timer还在,就会被清除,然后设置新的,相当于重置时间
-     */
     clearTimeout(timer);
     timer = setTimeout(() => {
       func.apply(this, args);
     }, delay);
+  }
+}
+
+// 节流
+export function throttle(fn,gapTime){
+  let _lastTime = 0;
+  return function(...args){
+    let _nowTime = +new Date() // 快速转为时间戳
+    if(_nowTime - _lastTime > gapTime){
+      fn.apply(this,args)
+      _lastTime = _nowTime
+    }
   }
 }
