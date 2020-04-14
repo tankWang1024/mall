26641049c66e905385f8ee0019ce2cfe21a3d807
wangtanke <1665497618@qq.com>
2020-04-14 14:31:25 +0800

    增加fastclick

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 
+import FastClick from 'fastclick'
+
 import Toast from './components/common/toast/index'
+
+// 解决移动端点击300ms延迟
+FastClick.attach(document.body)
 Vue.use(Toast)
 
 Vue.config.productionTip = false
